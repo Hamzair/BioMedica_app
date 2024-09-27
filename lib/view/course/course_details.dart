f585e1ab8a2cae8a828d8899b86a5c6de2053a0e
Midhat882 <syedmidhatg@gmail.com>
2024-09-27 19:19:29 +0500

    UI ...

--- lib/view/course/course_details.dart
+++ lib/view/course/course_details.dart
@@ -51,7 +51,7 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
                       image: widget.image));
                 }else{
                   if(widget.isPremium){
-                    Get.snackbar("title", "message");
+                    Get.snackbar("Locked", "Only for premium members");
                   }else{
                     Get.to(() => CourseContent(
                         title: widget.title,
